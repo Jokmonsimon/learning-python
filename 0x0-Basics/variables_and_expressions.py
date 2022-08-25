75833ef79339b59bf79c26952a7c7e9f10cd9693
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-08-25 19:24:50 +0300

    Variable and expressions

---
+++ 0x0-Basics/variables_and_expressions.py
@@ -0,0 +1,19 @@
+# Example file for Variables and Expressions
+# LinkedIn Learning Python course by Joe Marini
+
+# Basic data types in Python: Numbers, Strings, Booleans, Sequences, Dictionaries, Lists, Tuples
+my_int = 502
+my_float = 12.91
+my_str = "My Learning Model"
+my_bool = True
+my_list = [0, 1, "Afande Ojok", 5.01, False]
+my_tuple = (1, 3, 5, 7, 9)
+my_dict = {"Course Intructor": "Afande Ojok","Course Name": "Introduction to Computer Literacy", "Description":"We believe by enrolling in this course, everyone can have access to online information"}
+
+print(my_int) # Interger value
+print(my_float) # Value with decimal point
+print(my_str) # Sequence of characters
+print(my_bool) # True or False
+print(my_list) # Include any data type in square bracket
+print(my_tuple) # Imutable - cannot be changed
+print(my_dict) # Maps uniques keys to its pair values
