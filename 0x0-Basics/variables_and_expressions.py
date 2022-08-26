5d14c7674daa67750ed14d4d0c586b00e3b71d9b
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-08-26 09:36:13 +0300

    Variables and expressions

--- 0x0-Basics/variables_and_expressions.py
+++ 0x0-Basics/variables_and_expressions.py
@@ -8,7 +8,7 @@ my_str = "My Learning Model"
 my_bool = True
 my_list = [0, 1, "Afande Ojok", 5.01, False]
 my_tuple = (1, 3, 5, 7, 9)
-my_dict = {"Course Intructor": "Afande Ojok","Course Name": "Introduction to Computer Literacy", "Description":"We believe by enrolling in this course, everyone can have access to online information"}
+my_dict = {"Instructor": "Afande Ojok","CourseName": "Introduction to Computer Literacy", "Description":"We believe by enrolling in this course, everyone can have access to online information"}
 
 print(my_int) # Interger value
 print(my_float) # Value with decimal point
@@ -25,6 +25,30 @@ print(my_int)
 # To access a member of a sequence type, use []
 print(my_list[-4])
 print(my_list[1])
+
+# Use slices to get parts of a sequence
 print(my_list[0:2])
 print(my_list[1:5:2])
 print(my_tuple[1:4])
+
+# Use slices to reverse sequence
+print(my_list[::-1])
+
+# Dictionaries are acced via keys
+print(my_dict["Instructor"])
+
+# ERROR: Variables of different types cannot be combined
+print("String concatenation " + str(1234))
+
+# Global Vs. Loacl variables in functions
+def someFunction():
+    global my_str # Tell Python my_str is a global variable
+    my_str = "def"
+    print(my_str)
+
+someFunction()
+print(my_str)    
+
+# Delete variable
+del my_str
+print(my_str)
