33b6dec390e0ecb2aafab5a982fc75467a112e5a
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-06 18:45:03 +0300

    Working with modules in Python

---
+++ 0x0-Basics/modules.py
@@ -0,0 +1,14 @@
+# Example file for working with modules
+# LinkedIn Learning Python course by Joe Marini
+
+# TODO: Import the math module, which contains features for working with math
+import math
+
+# TODO: The math module contains lots of pre-built functions
+print("The square root of 16 is:", math.sqrt(16))
+
+# TODO: In addition to functions, some modules contain useful constants
+print("Pi is:", math.pi)
+
+# TODO: Try some of the math functions for yourself here:
+print("The value of 8 raised to power 3 is:", math.pow(8, 3))
